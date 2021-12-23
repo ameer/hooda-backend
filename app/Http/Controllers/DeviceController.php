384a4051e1f497029266e5a401476e205eb4af0e
ameer <xerxes1718@gmail.com>
2021-12-23 18:06:41 +0330

    update devices

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\device;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = $request->user();
+        $device = new device();
+        $device->owner_id = $user->id;
+        $
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        $deviceData = device::where('owner_id', $user->id)->get();
+        return response()->json($deviceData);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(device $device)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(device $device)
+    {
+        //
+    }
+}
