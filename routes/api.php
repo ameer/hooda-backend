980ee937e98cb69afdb82afbc5a172dcb5e61a95
ameer <xerxes1718@gmail.com>
2022-06-11 22:46:29 +0430

    update

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:6,1')->get('/app/version', function (Request $request) {
     return response()->json([
-        'version' => '1.0.1',
+        'version' => '1.0.3',
         'build' => '1',
         'needsUpdate' => true,
         'showMessage' => false,
